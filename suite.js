437b3b80bd3e666b5a1839114f67b049371a0e6b
Michal Novák <michal.novak@weborama.cz>
2017-12-04 13:30:57 +0100

    add transferFrom() tests

--- suite.js
+++ suite.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import {expectRevertOrFail} from './helpers'
+import { expectRevertOrFail } from './helpers'
 
 export default function suite(options) {
 	const accounts = options.accounts
@@ -155,6 +155,110 @@ export default function suite(options) {
 				}
 
 				let result = await token.transfer(to, amount, { from: from })
+				let log = result.logs[0]
+				assert.equal(log.event, 'Transfer')
+				assert.equal(log.args.from, from)
+				assert.equal(log.args.to, to)
+				expect(log.args.value).to.be.bignumber.equal(amount)
+			}
+		})
+
+		describe('transferFrom(_from, _to, _value)', function () {
+			beforeEach(async function () {
+				// by default approve bob to withdraw from alice
+				await token.approve(bob, 3, { from: alice })
+			})
+
+			it('should return true when called with amount of 0 and sender is approved', async function () {
+				assert.isTrue(await token.transferFrom.call(bob, alice, 0, { from: alice }))
+			})
+
+			it('should return true when called with amount of 0 and sender is not approved', async function () {
+				assert.isTrue(await token.transferFrom.call(alice, bob, 0, { from: bob }))
+			})
+
+			it('should revert when trying to transfer something while _from having nothing', async function () {
+				await expectRevertOrFail(token.transferFrom(alice, bob, 1, { from: bob }))
+			})
+
+			it('should revert when trying to transfer more than balance of _from', async function () {
+				await purchase(alice, 3)
+				await expectRevertOrFail(token.transferFrom(alice, bob, 4, { from: bob }))
+
+				await token.transferFrom(alice, bob, 1, { from: bob })
+				await expectRevertOrFail(token.transferFrom(alice, bob, 3, { from: bob }))
+			})
+
+			it('should revert when trying to transfer while not allowed at all', async function () {
+				await purchase(bob, 3)
+				await expectRevertOrFail(token.transferFrom(bob, alice, 1, { from: alice }))
+				await expectRevertOrFail(token.transferFrom(bob, alice, 2, { from: alice }))
+				await expectRevertOrFail(token.transferFrom(bob, alice, 3, { from: alice }))
+			})
+
+			it('should revert when trying to transfer more than allowed', async function () {
+				await purchase(bob, 3)
+				await token.approve(alice, 2, { from: bob })
+				await expectRevertOrFail(token.transferFrom(bob, alice, 3, { from: alice }))
+			})
+
+			it('should return true when transfer can be made', async function () {
+				await purchase(alice, 3)
+				assert.isTrue(await token.transferFrom.call(alice, bob, 1, { from: bob }))
+				assert.isTrue(await token.transferFrom.call(alice, bob, 2, { from: bob }))
+				assert.isTrue(await token.transferFrom.call(alice, bob, 3, { from: bob }))
+
+				await token.transferFrom(alice, bob, 1, { from: bob })
+				assert.isTrue(await token.transferFrom.call(alice, bob, 1, { from: bob }))
+				assert.isTrue(await token.transferFrom.call(alice, bob, 2, { from: bob }))
+			})
+
+			it('should update balances accordingly', async function () {
+				await purchase(alice, 3)
+				await token.transferFrom(alice, bob, 3, { from: bob })
+				expect(await token.balanceOf.call(bob)).to.be.bignumber.equal(3)
+				expect(await token.balanceOf.call(alice)).to.be.bignumber.equal(0)
+			})
+
+			it('should not affect totalSupply', async function () {
+				await purchase(alice, 3)
+				let supply1 = await token.totalSupply.call()
+				await token.transferFrom(alice, bob, 3, { from: bob })
+				let supply2 = await token.totalSupply.call()
+				expect(supply2).to.be.be.bignumber.equal(supply1)
+			})
+
+			it('should fire Transfer event', async function () {
+				await testTransferEvent(alice, bob, 3)
+			})
+
+			it('should fire Transfer event when transferring amount of 0', async function () {
+				await testTransferEvent(alice, bob, 0)
+			})
+
+			it('should fire Transfer event when transferring amount of 0 and sender is not approved', async function () {
+				await testTransferEvent(bob, alice, 0)
+			})
+
+			it('should transfer given amount', async function () {
+				await purchase(alice, 3)
+				let balance1 = await token.balanceOf.call(bob)
+
+				await token.transferFrom(alice, bob, 1, { from: bob })
+				let balance2 = await token.balanceOf.call(bob)
+				expect(balance2).to.be.bignumber.equal(balance1.plus(1))
+
+				await token.transferFrom(alice, bob, 2, { from: bob })
+				let balance3 = await token.balanceOf.call(bob)
+				expect(balance3).to.be.bignumber.equal(balance2.plus(2))
+			})
+
+			let testTransferEvent = async function (from, to, amount) {
+				if (amount > 0) {
+					await purchase(from, amount)
+				}
+
+				let result = await token.transferFrom(from, to, amount, { from: to })
 				let log = result.logs[0]
 				assert.equal(log.event, 'Transfer')
 				assert.equal(log.args.from, from)
