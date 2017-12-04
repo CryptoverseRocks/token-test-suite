80999d6d6bcac9cca1dff0d1e248b07e9a40a2cd
Michal Novák <michal.novak@weborama.cz>
2017-12-04 13:17:06 +0100

    add totalSupply() tests

--- suite.js
+++ suite.js
@@ -1,5 +1,44 @@
 'use strict'
 
 export default function suite(options) {
-	console.log('ERC-20 Token Standard Test Suite')
+	const accounts = options.accounts
+
+	// configure
+	const initialSupply = options.initialSupply || new web3.BigNumber(0)
+	const createToken = options.token
+	const purchase = function (to, amount) { return options.purchase(token, to, amount) }
+
+	// setup
+	const expect = require('chai')
+		.use(require('chai-bignumber')(web3.BigNumber))
+		.expect
+
+	// name the accounts
+	const alice = accounts[1]
+	const bob = accounts[2]
+
+	let token = null
+
+	beforeEach(async function () {
+		token = await createToken()
+	})
+
+	describe('ERC-20', function () {
+		describe('totalSupply()', function () {
+			it('should have initial supply of ' + initialSupply.toFormat(), async function () {
+				expect(await token.totalSupply.call()).to.be.bignumber.equal(initialSupply)
+			})
+
+			it('should return the correct supply', async function () {
+				await purchase(alice, 1)
+				expect(await token.totalSupply.call()).to.be.bignumber.equal(initialSupply.plus(1))
+
+				await purchase(alice, 2)
+				expect(await token.totalSupply.call()).to.be.bignumber.equal(initialSupply.plus(3))
+
+				await purchase(bob, 3)
+				expect(await token.totalSupply.call()).to.be.bignumber.equal(initialSupply.plus(6))
+			})
+		})
+	})
 }
